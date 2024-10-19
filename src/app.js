1bfc75b298d3be4dc61092785bdc493cbb0b8dc8
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-19 19:54:30 +0530

    Adding DB Models

--- src/app.js
+++ src/app.js
@@ -4,21 +4,24 @@ import {createServer} from "node:http";
 import {Server} from "socket.io";
 
 import mongoose from "mongoose";
+import { connectToSocket } from './controllers/socketmanager.js';
 
 import cors from "cors";
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = connectToSocket(server);
 
 app.set("port", (process.env.PORT || 8000))
+app.use(cors());
+app.use(express.json({limit: "40kb"}))
+app.use(express.urlencoded({limit: "40kb", extended: true}));
 
-
-app.get("/home", (req, res) => {
-    return res.json({"hello": "world"});
-})
 
 const start = async () => {
+    const connectionDb = await mongoose.connect("mongodb+srv://pushpendr7409:22aub7F6DuqG2oZz@zoom.fpuqi.mongodb.net/")
+
+    console.log(`Connected MongoDB Host: ${connectionDb.connection.host}`)
      server.listen (8000, () => {
         console.log("listening on port 8000")
      });
