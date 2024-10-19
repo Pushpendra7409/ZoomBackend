e6770ae17beeb359567414daa568e2f0d67ee6c0
Pushpendra7409 <pushpendra7409@gmail.com>
2024-10-19 20:44:33 +0530

    Authentication route

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import mongoose from "mongoose";
 import { connectToSocket } from './controllers/socketmanager.js';
 
 import cors from "cors";
+import userRoutes from "./routes/user.routes.js";
 
 const app = express();
 const server = createServer(app);
@@ -16,6 +17,8 @@ app.set("port", (process.env.PORT || 8000))
 app.use(cors());
 app.use(express.json({limit: "40kb"}))
 app.use(express.urlencoded({limit: "40kb", extended: true}));
+
+app.use("/api/v1/users", userRoutes);
 
 
 const start = async () => {
